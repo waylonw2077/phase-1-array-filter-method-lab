72977ce3c717a16a7cbd230de225a7f4562c0aed
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-27 14:49:09 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, str) {
+    const lowerCaseStr = str.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseStr));
+  }
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
